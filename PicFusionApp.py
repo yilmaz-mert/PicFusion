caafb06386bdf199159f74a2e181330a80bbdbfc
Mert.Y <mertyilmazgroup@gmail.com>
2025-01-05 11:35:59 +0300

    main app, works

---
+++ PicFusionApp.py
@@ -0,0 +1,72 @@
+import sys
+import ctypes
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QFileDialog, QListWidget, QPushButton
+)
+from PyQt6.QtGui import QIcon
+from PIL import Image
+from PicFusion_ui import Ui_MainWindow
+
+
+def set_app_user_model_id(app_id: str):
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
+
+
+class ImageMergerApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowIcon(QIcon('icon.ico'))
+
+        self.add_button = self.findChild(QPushButton, 'addButton')
+        self.remove_button = self.findChild(QPushButton, 'removeButton')
+        self.merge_button = self.findChild(QPushButton, 'mergeButton')
+        self.image_list = self.findChild(QListWidget, 'imageListWidget')
+
+        self.add_button.clicked.connect(self.add_images)
+        self.remove_button.clicked.connect(self.remove_selected_images)
+        self.merge_button.clicked.connect(self.merge_and_save_images)
+
+    def add_images(self):
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
+        file_dialog.setNameFilter("Images (*.png *.jpg *.jpeg *.bmp)")
+
+        if file_dialog.exec():
+            selected_files = file_dialog.selectedFiles()
+            self.image_list.addItems(selected_files)
+
+    def remove_selected_images(self):
+        for item in self.image_list.selectedItems():
+            self.image_list.takeItem(self.image_list.row(item))
+
+    def merge_and_save_images(self):
+        image_paths = [self.image_list.item(i).text() for i in range(self.image_list.count())]
+        if not image_paths:
+            return
+
+        image_objects = [Image.open(path) for path in image_paths]
+
+        widths, heights = zip(*(img.size for img in image_objects))
+        total_height = sum(heights)
+        max_width = max(widths)
+
+        merged_image = Image.new('RGB', (max_width, total_height))
+        y_offset = 0
+        for img in image_objects:
+            merged_image.paste(img, (0, y_offset))
+            y_offset += img.height
+
+        file_dialog = QFileDialog()
+        save_path, _ = file_dialog.getSaveFileName(self, "Save Merged Image", "", "Images (*.png *.jpg *.jpeg *.bmp)")
+
+        if save_path:
+            merged_image.save(save_path)
+
+
+if __name__ == "__main__":
+    set_app_user_model_id("PicFusionApp")
+    app = QApplication(sys.argv)
+    window = ImageMergerApp()
+    window.show()
+    sys.exit(app.exec())
